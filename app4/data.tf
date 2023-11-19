84be4d28b6965d98b816c295d7ee889d6cc2702c
boumkwa <mbestepaul10@gmail.com>
2023-11-19 18:28:52 -0500

    week12

---
+++ app4/data.tf
@@ -0,0 +1,25 @@
+
+data "aws_instance" "demo" {
+  instance_id = "i-0305c74c6d8c3c21c"
+
+}
+
+data "aws_ami" "ami1" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
---
+++ app5/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_instance.demo
+  id = "i-0305c74c6d8c3c21c"
+}
